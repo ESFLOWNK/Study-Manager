fb610aa9b5c93def8aec4055b9e0c979a48baa44
ESFLOWNK <otakuflownk@gmail.com>
2025-02-22 16:05:11 -0400

    handleOptions * Add a small detail

--- handleOptions.py
+++ handleOptions.py
@@ -1,7 +1,7 @@
 def pedirOpcion(desde: int,hasta: int) -> int:
     op = "" # Crea una variable que contiene las opciones ingresadas
-    if hasta == 0:
-        return -1
+    if desde+hasta == 0: # Si desde y hasta son 0
+        return -1        # Devuelve -1
     while not op in [str(i) for i in range(desde,hasta+1)]:
         # Revisa que op sea digitos y que sea mayor que 0 o menor que 4
         # De no ser asi se repite el proceso
